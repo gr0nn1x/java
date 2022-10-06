fc645d799797476563179b201cae97c7d1a6d465
gr0nn1x <100243642+gr0nn1x@users.noreply.github.com>
2022-10-06 09:28:49 +0200

    Add files via upload

---
+++ cikly/src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.print(i +  ": " );
+            for (int j = 1; j <=10; j++){
+                System.out.print(i * j + " ");
+            }
+
+            System.out.println("");
+        }
+
+
+    }
+}
