99c14612473e0735c263b5008830c903f8e07705
gr0nn1x <100243642+gr0nn1x@users.noreply.github.com>
2022-10-11 16:15:54 +0200

    Add files via upload

--- cikly/src/Main.java
+++ cikly/src/Main.java
@@ -1,17 +1,17 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        for (int i = 1; i <= 10 ; i++) {
-            System.out.print(i +  ": " );
-            for (int j = 1; j <=10; j++){
-                System.out.print(i * j + " ");
-            }
-
-            System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadej text jaký chceš vypsat");
+        String x = sc.nextLine();
+        System.out.println("Kolikrát ho chceš vypsat");
+        int y = sc.nextInt();
+        for (int i = 0; i < y ; i++) {
+            System.out.println(x);
         }
 
 
-    }
+
+    }{}
 }
